c279a6f05d811e1670b67bd1f068489fef45ab9a
David Tan <dtan@luc.edu>
2016-06-05 14:35:17 -0500

    add hackerank challenges too

---
+++ src/main/scala-2.11/Hackerrank/Strings/Pangrams.scala
@@ -0,0 +1,33 @@
+//https://www.hackerrank.com/challenges/pangrams
+object Solution {
+
+  def main(args: Array[String]) {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution
+*/
+    def helper(str: String): String = {
+      var joinString = ""
+      str.split("\\s+").
+        foreach(x => joinString += x.toLowerCase())
+
+      val map1 = joinString.mkString(",")
+
+        .map(x => (x, 1)).groupBy(_._1).
+
+        map(x => (x._1, x._2.length)).
+
+        toSeq.
+
+        sorted
+
+        .size
+      //println("SIZE  "+map1)
+      if (map1 == 26+1) return "pangram"
+      "not pangram"
+    }
+
+    val str= scala.io.StdIn.readLine()
+    //println(str)
+    println(helper(str))
+    //helper(str)
+  }
+}
